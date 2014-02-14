71f40ae0770b079e9f1deab3934449737a7ecb66
Juan Castro Fernández <castrofernandez@gmail.com>
2014-02-14 16:03:22 +0100

    Ejercicio 4

--- base/js/script.js
+++ base/js/script.js
@@ -6,3 +6,13 @@ else
 function iniciar() {
 	console.log("iniciar");
 }
+
+function boton1Pulsado() {
+	var div = document.getElementById("miDiv");
+	div.className = "clase1";
+}
+
+function boton2Pulsado() {
+	var div = document.getElementById("miDiv");
+	div.className = "clase2";
+}
