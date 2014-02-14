82153b7ae5c844f84baf28155bf7324c584c7952
Juan Castro Fernández <castrofernandez@gmail.com>
2014-02-14 16:29:37 +0100

    Error

--- base/js/script.js
+++ base/js/script.js
@@ -5,16 +5,4 @@ else
 	
 function iniciar() {
 	console.log("iniciar");
-	
-	document.getElementById("boton1").onclick = botonPulsado;
-	document.getElementById("boton2").onclick = botonPulsado;
-}
-
-function botonPulsado(event) {
-	var div = document.getElementById("miDiv");
-
-	if (this.id == "boton1")
-		div.className = "clase1";
-	else
-		div.className = "clase2";
 }
