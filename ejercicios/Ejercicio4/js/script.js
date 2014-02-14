9e0882d56e6d9db60ce1e671200f6bc015e155f8
Juan Castro Fernández <castrofernandez@gmail.com>
2014-02-14 16:38:10 +0100

    Ejercicio 4 versión 1

--- ejercicios/Ejercicio4/js/script.js
+++ ejercicios/Ejercicio4/js/script.js
@@ -5,4 +5,16 @@ else
 	
 function iniciar() {
 	console.log("iniciar");
+	
+	document.getElementById("boton1").onclick = botonPulsado;
+	document.getElementById("boton2").onclick = botonPulsado;
+}
+
+function botonPulsado(event) {
+	var div = document.getElementById("miDiv");
+
+	if (this.id == "boton1")
+		div.className = "clase1";
+	else
+		div.className = "clase2";
 }
